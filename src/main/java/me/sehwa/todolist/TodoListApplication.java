da86a05a2ae2d5082e6c3ae6da7be2cc71b2160b
SehwaKim <sehwakm@gmail.com>
2019-01-02 04:32:37 +0900

    정적자원 찾는 경로 추가

--- src/main/java/me/sehwa/todolist/TodoListApplication.java
+++ src/main/java/me/sehwa/todolist/TodoListApplication.java
@@ -2,13 +2,20 @@ package me.sehwa.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TodoListApplication {
+public class TodoListApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoListApplication.class, args);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/templates/public/static/");
+    }
 }
 
